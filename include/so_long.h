6d2ca3b065ae0c866923ec41b788d4ec9b7e49fc
pdlmn <jseldardw@gmail.com>
2024-02-28 19:52:21 +0100

    feat: updated includes to reflect changes

--- include/so_long.h
+++ include/so_long.h
@@ -6,13 +6,24 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 09:51:11 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/28 10:05:49 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/28 19:46:29 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include "fcntl.h"
+# include <X11/keysym.h>
+# include <fcntl.h>
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
+
+typedef struct	s_map
+{
+	char	**rows;
+	int		x;
+	int		y;
+}				t_map;
+
+void	parse(char *file, t_map *map);
+
 #endif 
