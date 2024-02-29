06f04718545d4ed2dd6c104ac095037c2ccbf7ab
pdlmn <jseldardw@gmail.com>
2024-02-29 13:05:02 +0100

    feat: updated header to reflect changes

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 09:51:11 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/28 19:46:29 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/29 10:25:43 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,27 @@
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 
+enum e_tokens
+{
+	empty_tile = '0',
+	wall_tile = '1',
+	player_tile = 'P',
+	collectible_tile = 'C',
+	exit_tile = 'E',
+	enemy_tile = 'V'
+};
+
 typedef struct	s_map
 {
 	char	**rows;
 	int		x;
 	int		y;
+	int		player_count;
+	int		collectible_count;
+	int		exit_count;
 }				t_map;
 
 void	parse(char *file, t_map *map);
+void	id(void *ptr);
 
 #endif 
